6931e94904da4b2bf6d212b5964f75e98e830b8f
Syntax-code <58822244+Syntax-code@users.noreply.github.com>
2020-12-05 23:17:47 +0530

    2 player game done need improvements like arr and entering the places taken error

--- Tic Tac Toe/tic_tac_toe.py
+++ Tic Tac Toe/tic_tac_toe.py
@@ -1,7 +1,28 @@
 def check(one, two, three, four, five, six, seven, eight, nine):
     global status
-    status = 'draw'
-
+    if((one == 'X') and (two == 'X') and (three == 'X')): #all conditions for winning 
+        status = 'Xwin'
+    elif((one == '0') and (two == '0') and (three == '0')):
+        status = '0win'
+    elif((four == 'X') and (five == 'X') and (six == 'X')):
+        status = 'Xwin'
+    elif((four == '0') and (five == '0') and (six == '0')):
+        status = '0win'
+    elif((seven == 'X') and (eight == 'X') and (nine == 'X')):
+        status = 'Xwin'
+    elif((seven == '0') and (eight == '0') and (nine == '0')):
+        status = '0win'
+    elif((one == 'X') and (five == 'X') and (nine == 'X')):
+        status = 'Xwin'
+    elif((one == '0') and (five == '0') and (nine == '0')):
+        status = '0win'
+    elif((three == 'X') and (five == 'X') and (seven == 'X')):
+        status = 'Xwin'
+    elif((three == '0') and (five == '0') and (seven == '0')):
+        status = '0win'
+    elif((one != '---') and (two != '---') and (three != '---') and (four != '---') and (five != '---') and (six != '---') and (seven != '---') and (eight != '---') and (nine != '---')):
+        status = 'draw'
+    
 def tic_tac(num, piece):   # num = for pos, piece = X / 0
     global one
     global two
@@ -38,13 +59,22 @@ def tic_tac(num, piece):   # num = for pos, piece = X / 0
     {}|{}|{}
     '''.format(one, two, three, four, five, six, seven, eight, nine))
     check(one, two, three, four, five, six, seven, eight, nine)
+    if status == 'Xwin':
+        print('X wins')
+    elif status == '0wins':
+        print('0 wins')
+    elif status == 'draw':
+        print('Its A Draw')
 
-def single_player(dine):
+def multi_player(dine):
     print('Get ready')
     tic_tac(1, '---')
-    while(status != 'win' or status != 'lose' or status != 'draw'):
+    while(status=='ongoing'):
         if dine == 'one':
             num = int(input('Enter your pos [1 - 9]'))
+            if num - 9 > 0 or num < 0:
+                print('Enter valid number')
+                continue
             tic_tac(num, 'X')
             dine = 'two'
         else:
@@ -53,13 +83,13 @@ def single_player(dine):
             dine = 'one'
         
 
-def multi_player():
+def comp_player():
     tic_tac()
 
-print('For Single Player Press s')
 print('For Double Player Press d')
+print('For a game with Computer Player Press c')
 mode = input('Please select mode ')
-one = '---'
+one = '---'                 #rather than using one , two we can use a array or list for simplisity
 two = '---'
 three = '---'
 four = '---'
@@ -70,7 +100,7 @@ eight = '---'
 nine = '---'
 status = 'ongoing'
 dine = 'one'
-if(mode == 's'):
-    single_player(dine)
+if(mode == 'd'):
+    multi_player(dine)
 else:
-    multi_player()
+    comp_player()
