cc6d5b804f8886ea60ec841a8c265baa144b8266
Syntax-code <58822244+Syntax-code@users.noreply.github.com>
2021-01-14 12:33:39 +0530

    optimized single player

--- Tic Tac Toe/tic_tac_toe.py
+++ Tic Tac Toe/tic_tac_toe.py
@@ -27,6 +27,7 @@ def is_winner(element, element_matrix):
 
 
 def is_empty(pos, element_matrix):
+    num = [0,1,2,3,4,5,6,7,8]
     if (pos != ''):
         if (element_matrix[int(pos)] == '-'):
             return True
@@ -35,33 +36,41 @@ def is_empty(pos, element_matrix):
     else:
         return False
 
+def two_player(element_matrix):
+    player_status = input('Press "s" to start the Game : ')
+    elements = ['X', '0']
+    element = random.choice(elements)
+    while(player_status == 's'):
+        if (element == 'X'):
+            pos = input("Enter the position to place X [ 0-8 ] : ")
+            if (bool(is_empty(pos, element_matrix)) == True):
+                element_matrix = put(pos, element, element_matrix)
+                show_game(element_matrix)
+                if (bool(is_winner(element, element_matrix)) == True):
+                    print("{} is the winner".format(element))
+                    break       
+                element = "0"
+            else:
+                print("The position has been taken please enter another position or enter a proper number ")
+                continue
+        else:
+            pos = input("Enter the position to place 0 [ 0-8 ] : ")
+            if (bool(is_empty(pos, element_matrix)) == True):
+                element_matrix = put(pos, element, element_matrix)
+                show_game(element_matrix)
+                if (bool(is_winner(element, element_matrix)) == True):
+                    print("{} is the winner".format(element))
+                    break       
+                element = "X"
+            else:
+                print("The position has been taken please enter another position or enter a proper number ")
+                continue
+
 print("X & 0")
 show_game(element_matrix)
-player_status = input('Press "s" to start the Game : ')
-elements = ['X', '0']
-element = random.choice(elements)
-while(player_status == 's'):
-    if (element == 'X'):
-        pos = input("Enter the position to place X [ 0-8 ] : ")
-        if (bool(is_empty(pos, element_matrix)) == True):
-            element_matrix = put(pos, element, element_matrix)
-            show_game(element_matrix)
-            if (bool(is_winner(element, element_matrix)) == True):
-                print("{} is the winner".format(element))
-                break       
-            element = "0"
-        else:
-            print("The position has been taken please enter another position ")
-            continue
-    else:
-        pos = input("Enter the position to place 0 [ 0-8 ] : ")
-        if (bool(is_empty(pos, element_matrix)) == True):
-            element_matrix = put(pos, element, element_matrix)
-            show_game(element_matrix)
-            if (bool(is_winner(element, element_matrix)) == True):
-                print("{} is the winner".format(element))
-                break       
-            element = "X"
-        else:
-            print("The position has been taken please enter another position ")
-            continue
+print("Enter the GameType :")
+print("1. Two Player mode : t")
+print("1. Random Computer mode : r")
+game_type = input("Enter your choice : ")
+if (game_type == 't'):
+    two_player(element_matrix)
