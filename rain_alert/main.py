83dd4f9548341861ab330d046b29c9b83440cdd2
NyuBoy <>
2021-05-10 10:12:15 +0000

    rain_alert

---
+++ rain_alert/main.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from twilio.rest import Client
+from twilio.http_client import TwilioHttpClient
+import os
+
+
+end_point = 'https://api.openweathermap.org/data/2.5/onecall'
+api_key = '2cf27db91ba310d0a4cb40c4a611c20e'
+account_sid = 'AC1170b135bf183ccd059e820039b8ca20'
+auth_token = '7cabb1d4121a69ff527677ac9cfbf39c'
+
+parameters = {
+    'lat' : 19.456360,
+    'lon' : 72.792458,
+    'exclude' : 'current,minutely,daily',
+    'appid' : api_key
+}
+
+will_rain = False
+
+response = requests.get(url = end_point, params = parameters)
+data = response.json()
+data = data['hourly'][:12]
+for each_hour_data in data:
+    condition_code = each_hour_data['weather'][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https' : os.environ['https_proxy']}
+    client = Client(account_sid, auth_token, http_client = proxy_client)
+
+    message = client.messages \
+                    .create(
+                        body="Its gonna rain.",
+                        from_='+14692948199',
+                        to='9579337529'
+                    )
+
+    print(message.status)
